3844b6b38eb02e6f86d0d8636f35c4c3f7e3bc10
Lehlogonolo Poole <hlonipoole692@gmail.com>
2024-04-21 18:22:43 +0200

    uhhh messy code

--- BP/scripts/funcs.ts
+++ BP/scripts/funcs.ts
@@ -15,6 +15,16 @@ function secondsToTicks(num: number): number {
     return num * 20
 }
 
+let re;
+const r = {
+    s: (func: () => void) => {
+        re = system.runInterval(func, secondsToTicks(4));
+    },
+    c: () => {
+        re = system.clearRun(re);
+    }
+}
+
 function doevery(p: Player, msg: string) {
     const [, name, t] = msg.split(" ");
     console.warn(t)
@@ -45,38 +55,44 @@ function doevery(p: Player, msg: string) {
     let shouldStop = false;
 
     // user took too long to respond, so remove event, maybe use [current < current + (timeout * 1000)] for calc
-    while (cont == false) {
-        if (run == undefined) {
-            run = system.runTimeout(() => {
-                error(p, "You took too long to respond. The command subscription has been canceled.");
-                shouldStop = true; // Set the flag variable to indicate that the function should stop.
-                cont = true;
-                return;
-            }, secondsToTicks(timeout));
-        }
-    }
-
-    world.afterEvents.chatSend.unsubscribe(event);
-
-    if (shouldStop) return;
-
-    // Rest of doevery code, finally.
-
-    ints[name] = system.runInterval(() => {
-        let count: number = 0;
-        try {
-            p.runCommand(executable)
-        } catch (err) {
-            count++
-            if (count > 5) {
-                delete ints[name]
-                error(p, `Task with the name (${name}) has failed more than 5 times. Execution has automatically been stopped and task has been cleared.`)
+    r.s(() => {
+        if (cont == false) {
+            if (run == undefined) {
+                run = system.runTimeout(() => {
+                    error(p, "You took too long to respond. The command subscription has been canceled.");
+                    world.afterEvents.chatSend.unsubscribe(event);
+                    shouldStop = true; // Set the flag variable to indicate that the function should stop.
+                    cont = true;
+                    return;
+                }, secondsToTicks(timeout));
             }
+        } else {
+
+            world.afterEvents.chatSend.unsubscribe(event);
+
+
+            if (shouldStop) return;
+
+            // Rest of doevery code, finally.
+
+            ints[name] = system.runInterval(() => {
+                let count: number = 0;
+                try {
+                    p.runCommand(executable)
+                } catch (err) {
+                    count++
+                    if (count > 5) {
+                        delete ints[name]
+                        error(p, `Task with the name (${name}) has failed more than 5 times. Execution has automatically been stopped and task has been cleared.`)
+                    }
+                }
+            }, secondsToTicks(time))
+
+            r.c();
+
+            p.sendMessage(`Task with the name (${name}) has been added!`)
         }
-    }, secondsToTicks(time))
-
-    p.sendMessage(`Task with the name (${name}) has been added!`)
-
+    })
 }
 
 function stopevery(p: Player, msg: string) {
