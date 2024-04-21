6649363365a616dd4c6c39e51b33bf64a9c082d3
Lehlogonolo Poole <hlonipoole692@gmail.com>
2024-04-21 15:49:58 +0200

    first commit

---
+++ BP/scripts/funcs.ts
@@ -0,0 +1,94 @@
+import { Player, system, world, ChatSendBeforeEvent } from "@minecraft/server"
+
+let ints: {
+    [key: string]: number
+} = {}
+let timeout = 20
+let run: number;
+let event: (arg: ChatSendBeforeEvent) => void;
+
+function error(p: Player, msg: string) {
+    p.sendMessage("§3" + msg);
+}
+
+function secondsToTicks(num: number): number {
+    return num * 20
+}
+
+function doevery(p: Player, msg: string) {
+    const [, name, t] = msg;
+    const time = parseInt(t);
+
+    if (isNaN(time)) {
+        error(p, "The time must be a valid number! (In seconds)")
+        return;
+    }
+    let executable: string;
+
+    p.sendMessage(`Send the chat command you want to execute every ${t} seconds. (Without the slash (/)`)
+
+    event = world.beforeEvents.chatSend.subscribe((a) => {
+        if (a.sender != p) return;
+        if (/^\W/.test(a.message)) {
+            error(p, `Command contained (${a.message[0]}) at the start, so it was ignored.`)
+        }
+
+        executable = a.message
+        system.clearRun(run)
+        world.beforeEvents.chatSend.unsubscribe(event);
+    })
+
+    let shouldStop = false;
+
+    // user took too long to respond, so remove event, maybe use [current < current + (timeout * 1000)] for calc
+    run = system.runTimeout(() => {
+        error(p, "You took too long to respond. The command subscription has been canceled.");
+        shouldStop = true; // Set the flag variable to indicate that the function should stop.
+        return;
+    }, secondsToTicks(timeout));
+
+    world.beforeEvents.chatSend.unsubscribe(event);
+
+    if (shouldStop) return;
+
+    // Rest of doevery code, finally.
+
+    ints[name] = system.runInterval(() => {
+        let count: number = 0;
+        try {
+            p.runCommand(executable)
+        } catch (err) {
+            count++
+            if (count > 5) {
+                delete ints[name]
+                error(p, `Task with the name (${name}) has failed more than 5 times. Execution has automatically been stopped and task has been cleared.`)
+            }
+        }
+    }, secondsToTicks(time))
+
+    p.sendMessage(`Task with the name (${name}) has been added!`)
+
+}
+
+function stopevery(p: Player, msg: string) {
+    const [, name] = msg;
+    if (!(name in ints)) error(p, `Task with the name (${name}) does not exist.`);
+
+    delete ints[name];
+    p.sendMessage("Task has successfully been cleared!")
+}
+
+function listevery(p: Player) {
+    const intervalList = Object.keys(ints).map(name => `§lTask Name: §r§o${name} | §lExecutable: §r§o${ints[name]}`);
+
+    if (intervalList.length == 0) {
+        p.sendMessage("No intervals have been set. yet.")
+    } else {
+        for (const i of intervalList) {
+            p.sendMessage(i)
+        }
+    }
+
+}
+
+export { doevery, stopevery, listevery }
---
+++ BP/scripts/main.ts
@@ -0,0 +1,30 @@
+import { world } from "@minecraft/server"
+import { doevery, stopevery, listevery } from "./funcs"
+
+world.beforeEvents.chatSend.subscribe((arg) => {
+    // !doevery name time
+    // !de name time
+    const p = arg.sender;
+    if (!p.isOp) return;
+
+    const message = arg.message
+    if (!/^\W/.test(message)) return; // This allows use for any prefix to be used. Any character that isnt 0-9 a-z A-Z works.
+
+    switch (message.substring(1).split(" ")[0].toLowerCase()) { // check first param
+        case "de":
+        case "doevery":
+            doevery(p, message.substring(1));
+            break;
+        case "stopevery":
+        case "se":
+        case "endevery":
+        case "ee":
+            stopevery(p, message.substring(1))
+            break;
+        case "listevery":
+        case "le":
+        case "list":
+            listevery(p)
+            break;
+    }
+})
